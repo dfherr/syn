1b0003d742decdc3393545b63a02833777b70cd3
msslx <michael.seibt@tum.de>
2015-09-09 15:19:36 +0200

    Added download for owners statuspage

--- tests/update_ressources.py
+++ tests/update_ressources.py
@@ -2,6 +2,7 @@ from unipath import Path
 
 from syn_utils import RES_DIR, generate_session
 from stats.rankings import link_syndicate
+from stats.owner_stats import link_home
 
 
 def update_captcha(session):
@@ -16,7 +17,14 @@ def update_syndicates(session):
         f.write(c)
 
 
+def update_home(session):
+    c = session.s.get(link_syndicate()).content
+    with open(Path(RES_DIR, 'home/home_basic.html'), 'w') as f:
+        f.write(c)
+
+
 if __name__ == '__main__':
     session = generate_session()
-    update_captcha(session)
-    update_syndicates(session)
+    # update_captcha(session)
+    # update_syndicates(session)
+    update_home(session)
