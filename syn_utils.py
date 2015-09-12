ac1711c33a90c6aede656bfa71f18b7ea6539b77
msslx <michael.seibt@tum.de>
2015-09-12 13:48:24 +0200

    Moved get_session from utils to LoggedInSession, added some links

--- syn_utils.py
+++ syn_utils.py
@@ -1,11 +1,10 @@
 import json
 from unipath import Path
 
-from login.login import LoggedInSession
-
 
 __all__ = ['BASE_DIR', 'TEST_DIR', 'RES_DIR',
-           'get_secret', 'generate_session', 'main_link']
+           'get_secret', 'string_to_int',
+           'main_link', 'overview_link']
 
 BASE_DIR = Path(__file__).resolve().ancestor(1)
 TEST_DIR = Path(BASE_DIR, 'tests')
@@ -19,27 +18,13 @@ def get_secret(setting, secrets=secrets):
     return secrets[setting]
 
 
-def generate_session():
-    """
-    Load the last session. Test if its still valid.
-    If the old session is valid, return it otherwise
-    generate a new one.
-    """
-    last_session = LoggedInSession.load_session()
-    if last_session is not None:
-        if last_session.successful_login():
-            return last_session
-
-    return LoggedInSession(
-        user=get_secret('user'),
-        password=get_secret('password')
-    )
-
-
 def string_to_int(x):
     return int(x.replace('.', ''))
 
 
-class InconsistencyException(Exception):
-    def __init__(self, message):
-        super(InconsistencyException, self).__init__(message)
+# Different Links
+def main_link(page):
+    return 'http://www.syndicates-online.de/{0}'.format(page)
+
+login_link = main_link('php/login.php')
+overview_link = main_link('php/statusseite.php')
