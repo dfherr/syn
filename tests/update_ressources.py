46e27538967fa8f2d0c398887738f2762a0734e7
msslx <michael.seibt@tum.de>
2015-09-13 23:25:09 +0200

    Applied structure changes

--- tests/update_ressources.py
+++ tests/update_ressources.py
@@ -1,8 +1,8 @@
 from unipath import Path
 
-from syn_utils import RES_DIR, generate_session
-from stats.rankings import link_syndicate
-from stats.owner_stats import link_home
+from login.login import LoggedInSession
+from syn_utils import overview_link, RES_DIR
+from stats.rankings import syndicate_link
 
 
 def update_captcha(session):
@@ -12,19 +12,19 @@ def update_captcha(session):
 
 
 def update_syndicates(session):
-    c = session.s.get(link_syndicate(11)).content
+    c = session.s.get(syndicate_link(11)).content
     with open(Path(RES_DIR, 'syndicate/new_syndicate.html'), 'w') as f:
         f.write(c)
 
 
 def update_home(session):
-    c = session.s.get(link_home()).content
+    c = session.s.get(overview_link).content
     with open(Path(RES_DIR, 'home/home_basic.html'), 'w') as f:
         f.write(c)
 
 
 if __name__ == '__main__':
-    session = generate_session()
-    # update_captcha(session)
-    # update_syndicates(session)
-    update_home(session)
+    with LoggedInSession.get_session() as session:
+        # update_captcha(session)
+        # update_syndicates(session)
+        update_home(session)
