19b608376a160c57f0a394da21cccd3499ec9af4
msslx <michael.seibt@tum.de>
2015-09-25 02:55:04 +0200

    moved links to api

--- syn_utils.py
+++ syn_utils.py
@@ -3,12 +3,15 @@ from unipath import Path
 
 
 __all__ = ['BASE_DIR', 'TEST_DIR', 'RES_DIR',
-           'get_secret', 'string_to_int',
-           'main_link', 'overview_link']
+           'sql_file', 'session_file',
+           'get_secret', 'string_to_int']
 
 BASE_DIR = Path(__file__).resolve().ancestor(1)
 TEST_DIR = Path(BASE_DIR, 'tests')
 RES_DIR = Path(TEST_DIR, 'resources')
+
+sql_file = Path(BASE_DIR, 'Database.sql')
+session_file = Path(BASE_DIR, 'last_session.pkl')
 
 with open(Path(BASE_DIR, 'secrets.json'), 'r') as f:
     secrets = json.loads(f.read())
@@ -22,13 +25,9 @@ def string_to_int(x):
     return int(x.replace('.', ''))
 
 
-# Different Links
+# Different Links # DEPRECATED! moved to API
 def main_link(page):
     return 'http://www.syndicates-online.de/{0}'.format(page)
-
-
-def syndicate_link(syn_number):
-    return main_link('php/syndicate.php?rid={0}'.format(syn_number))
 
 login_link = main_link('php/login.php')
 overview_link = main_link('php/statusseite.php')
