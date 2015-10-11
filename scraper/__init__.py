b7ea0e3596d87e99aebf141ad395ae296f739314
msslx <michael.seibt@tum.de>
2015-10-12 00:58:33 +0200

    added global market parsing

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -1,4 +1,7 @@
 from .store import scrape_store
 from .owner_stats import scrape_owner_stats
+from .global_market import scrape_market_resources
+from .rankings import generate_user_rankings, scrape_syndicate
 
-__all__ = ['scrape_owner_stats', 'scrape_store']
+__all__ = ['generate_user_rankings', 'scrape_market_resources', 'scrape_owner_stats',
+           'scrape_syndicate', 'scrape_store']
