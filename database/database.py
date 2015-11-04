44e9123a5b118464715dd9c6fea385598fb2c530
msslx <michael.seibt@tum.de>
2015-11-04 14:12:19 +0100

    complete rework and added functionality

--- database/database.py
+++ database/database.py
@@ -1,3 +1,4 @@
+from datetime import datetime as dt
 import sqlite3 as sql
 
 from settings import sql_file
@@ -15,26 +16,133 @@ class Database(object):
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.con.close()
 
+    def _create_table(self, name, create_string):
+        self.con.execute("DROP TABLE IF EXISTS {0}".format(name))
+        self.con.execute(create_string.format(name))
+        self.con.commit()
+
     def create_database(self):
         """
         Creates a new Database. Careful - it will overwrite your
-        old data if there is an old database with the same table name!
+        old data!
+        """
+        self.create_table_actions()
+        self.create_table_rankings()
+        self.create_table_shares()
+        self.create_table_storage()
+
+    def create_table_storage(self):
+        """
+        Table to save storage exchange rates
         """
         create_string = """
-                CREATE TABLE rankings (
-                    id INTEGER PRIMARY KEY NOT NULL,
-                    rank_date TIMESTAMP NOT NULL,
-                    current_rank INTEGER NOT NULL,
-                    name TEXT NOT NULL,
-                    class TEXT NOT NULL,
-                    ha INTEGER NOT NULL,
-                    nw INTEGER NOT NULL,
-                    syn INTEGER NOT NULL)"""
-        self.con.execute("DROP TABLE IF EXISTS rankings")
-        self.con.execute(create_string)
+            CREATE TABLE {0} (
+            id INTEGER PRIMARY KEY NOT NULL,
+            storage_date TIMESTAMP NOT NULL,
+            energy DOUBLE NOT NULL,
+            erz DOUBLE NOT NULL,
+            fp DOUBLE NOT NULL)"""
+        self._create_table('storage', create_string)
+
+    def create_table_rankings(self):
+        """
+        Table to save nw / ha rankings in
+        """
+        create_string = """
+            CREATE TABLE {0} (
+            id INTEGER PRIMARY KEY NOT NULL,
+            rank_date TIMESTAMP NOT NULL,
+            current_rank INTEGER NOT NULL,
+            name TEXT NOT NULL,
+            class TEXT NOT NULL,
+            ha INTEGER NOT NULL,
+            nw INTEGER NOT NULL,
+            syn INTEGER NOT NULL)"""
+        self._create_table('rankings', create_string)
+
+    def create_table_actions(self):
+        """
+        Table to save bot actions / output
+        """
+        create_string = """
+            CREATE TABLE {0} (
+            id INTEGER PRIMARY KEY NOT NULL,
+            action_date TIMESTAMP NOT NULL,
+            action TEXT NOT NULL)"""
+        self._create_table('actions', create_string)
+
+    def create_table_shares(self):
+        """
+        Table to save shares stats
+        """
+        create_string = """
+            CREATE TABLE {0} (
+            id INTEGER PRIMARY KEY NOT NULL,
+            shares_date TIMESTAMP NOT NULL,
+            syn INTEGER NOT NULL,
+            owning INTEGER NOT NULL,
+            percentage_owning DOUBLE NOT NULL,
+            overall INTEGER NOT NULL,
+            price INTEGER NOT NULL)"""
+        self._create_table('shares', create_string)
+
+    def action_output(self, x, date):
+        """
+        prints and saves an bot action / output
+        with formated datetime
+        """
+        print('{0}: {1}'.format(dt.strftime(date, "%H:%M"), x))
+        self.cur.execute(
+            'INSERT INTO actions '
+            '(action_date, action) '
+            'VALUES (?, ?)',
+            [date, x]
+        )
         self.con.commit()
 
-    def save_rankings(self, rankings):
+    def save_shares(self, shares, date):
+        """
+        saves the scraped share amounts / prices etc.
+        from scraper.shares in form of
+
+        {syn: [owning, percentage_owning, amount, price], ...}
+
+        to the database table 'shares':
+        | ID | DATE | SYN | OWNING | PERCENTAGE_OWNING | OVERALL | PRICE |
+        """
+        tmp = []
+        for key in shares.keys():
+            tmp.append([key, date] + shares[key])
+        # TODO: Bulk upload
+        for syn in tmp:
+            self.cur.execute(
+                'INSERT INTO shares '
+                '(shares_date, syn, owning, percentage_owning, overall, price) '
+                'VALUES (?, ?, ?, ?, ?, ?)',
+                syn
+            )
+            self.con.commit()
+
+    def save_storage(self, store, date):
+        """
+        saves the scraped storage exchange rates
+        from scraper.store in form of
+
+        [(datetime.now(), 'energy', 'erz', 'fp')]
+
+        to the database table 'storage':
+        | ID | DATE | ENERGY | ERZ | FP |
+        """
+        tmp = [date, store['ex_energy'], store['ex_erz'], store['ex_fp']]
+        self.cur.execute(
+            'INSERT INTO storage '
+            '(storage_date, energy, erz, fp) '
+            'VALUES (?, ?, ?, ?)',
+            tmp
+        )
+        self.con.commit()
+
+    def save_rankings(self, rankings, date):
         """
         saves the scraped and ordered rankings
         from scraper.rankings in form of
@@ -45,6 +153,9 @@ class Database(object):
         to the database table 'rankings':
         | ID | DATE | RANK | NAME | CLASS | HA | NW | SYN |
         """
+        for i in range(len(rankings)):
+            rankings[i] = [date, i+1] + rankings[i]
+
         # TODO: bulk upload...
         for rank in rankings:
             self.cur.execute(
