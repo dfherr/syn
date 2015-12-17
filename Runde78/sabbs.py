f9c7934a0b7218ed8d4ac2386650ada727320952
msslx <michael.seibt@tum.de>
2015-12-17 14:13:56 +0100

    fixed dp multiplicator

--- Runde78/sabbs.py
+++ Runde78/sabbs.py
@@ -26,22 +26,28 @@ def dp(ha, spys=None, ctp=False, hn=3, issdn=3, syn_issdn=0.52, sabbs=0):
 
     return (base_dp/ha+bonus_dp)*(1 + hn*0.1 + issdn*0.1 + syn_issdn + sabbs*0.09)
 
-if __name__ == '__main__':
-    thiefs = 240000
-    grab = thiefs * 0.015
-    print('Maxgrab: {0}'.format(grab))
 
-    off_spys = [thiefs, 0, 0]
-    def_spys = [26000, 370000, 5000]
-    # def_spys = [0, 0, 21500*(15+0.8*3)]
+def mili_sabbs(off_ha, off_spys, def_ha, def_spys, n):
+    grab = off_spys[0] * 0.015 +  (off_spys[1]+off_spys[2]) * 0.0075
+    print('Maxgrab: {0}'.format(grab))
 
     s = 0
     sabbs = 0
-    for i in range(int(3* 65/3)):
-        prob = 1-dp(21600, def_spys, sabbs=int(sabbs))/(2*op(6200, off_spys))
+    for i in range(n):
+        prob = 1-(1.5*dp(def_ha, def_spys, sabbs=int(sabbs)))/(2*op(off_ha, off_spys))
         sabbs += prob
         if prob < 0.01:
             prob = 0.01
         ev_per_sabb = prob*grab
         s += ev_per_sabb
         print('{0}:\t {1:.4f} \t {2:07.2f} \t {3:08.2f}'.format(i, prob, ev_per_sabb, s))
+
+
+if __name__ == '__main__':
+    off_ha = 6200
+    off_spys = [240000, 0, 0]
+
+    def_ha = 21600
+    def_spys = [26000, 570000, 5000]
+
+    mili_sabbs(off_ha, off_spys, def_ha, def_spys, 3 * 65//3)
