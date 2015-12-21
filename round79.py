5c681ee63a7d642a2315dcf85db9ef46ed43174e
msslx <michael.seibt@tum.de>
2015-12-21 23:10:45 +0100

    Skeleton for a prodder bot

--- round79.py
+++ round79.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python
 
-# take log
-# use cr -> gebs, ranger?, ha up to limit
+from intelligence import BaseBot, StatsBot
 
 
-# TODO: build statsbot
-# TODO: inherit from statsbot
-# TODO: simulating ha costs
-# TODO: buy energy
-# TODO: fos tree boost?!
+class NEBBot(BaseBot, StatsBot):
+    def __init__(self):
+        super(NEBBot, self).__init__()
+        self.last_hour = self.last_stats()
 
+        self.active = True
+        self.sleep_time = 30
 
-class ProdderStatsBot(object):
-    pass
+        # self.jobs.append(self.spend_cr)
+        # self.jobs.append(self.buy_resources)
+
+        self.hourly_jobs.append(self.take_log)
+        # self.hourly_jobs.append(self.sell_open_prod)
+
+    def sell_open_prod(self):
+        """ Reasonable part of open prod should be in erz """
+        raise NotImplementedError
+
+    def spend_cr(self):
+        """ Gebs, HUC(OC?), Spies, Ha, Store ->
+        how much erz can get sold each tick? """
+        raise NotImplementedError
+
+    def buy_resources(self):
+        """ Energy & FP for next tick """
+        raise NotImplementedError
+
+if __name__ == '__main__':
+    bot = NEBBot()
+    bot.run()
