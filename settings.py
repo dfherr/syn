8f5f0c1fc9ba66b1731a76d81b5a4d37b3c18801
msslx <michael.seibt@tum.de>
2016-01-02 14:05:10 +0100

    Round 79 changes

--- settings.py
+++ settings.py
@@ -2,14 +2,14 @@ import json
 from unipath import Path
 
 
-__all__ = ['BASE_DIR', 'TEST_DIR', 'RES_DIR', 'sql_file',
+__all__ = ['BASE_DIR', 'TEST_DIR', 'RES_DIR', 'current_db_file',
            'session_file', 'syns_amount', 'get_secret']
 
 BASE_DIR = Path(__file__).resolve().ancestor(1)
 TEST_DIR = Path(BASE_DIR, 'tests')
 RES_DIR = Path(TEST_DIR, 'resources')
 
-sql_file = Path(BASE_DIR, 'Database.sql')
+current_db_file = Path(BASE_DIR, 'Database.sql')
 session_file = Path(BASE_DIR, 'last_session.pkl')
 
 syns_amount = 27
